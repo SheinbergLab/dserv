14e69ab2f0028999a60b8a80a95e7c2086bb08b4
David Sheinberg <David_Sheinberg@brown.edu>
2024-06-14 19:08:22 -0400

    first pass at input requests

--- modules/gpio_input/src/gpio_input.c
+++ modules/gpio_input/src/gpio_input.c
@@ -35,20 +35,25 @@
 
 typedef struct gpio_input_s
 {
+  int line;
 #ifdef __linux__  
   struct gpioevent_request req;
   struct epoll_event ev;
   pthread_t input_thread_id;
+  int epfd;			/* epoll fd */
 #endif
-  char *dpoint_name;
+  tclserver_t *tclserver;
+  char *dpoint_prefix;
 } gpio_input_t;
   
 typedef struct gpio_info_s
 {
   int fd;			/* chip fd */
   int nlines;
+  tclserver_t *tclserver;
+  char *dpoint_prefix;
 #ifdef __linux__
-  struct gpio_input_t **input_requests;
+  gpio_input_t **input_requests;
 #endif
 } gpio_info_t;
 
@@ -64,23 +69,43 @@ void *input_thread(void *arg)
 
   struct epoll_event ev;
   ev.events = EPOLLIN;
-  ev.data.fd = info->req->fd;
-  int epfd = epoll_create(1);
-  int res = epoll_ctrl(epfd, EPOLL_CTL_ADD, info->req->fd, &ev);
-  int nfds, res;
+  ev.data.fd = info->req.fd;
+  info->epfd = epoll_create(1);
+  int res = epoll_ctl(info->epfd, EPOLL_CTL_ADD, info->req.fd, &ev);
+  int nfds, nread;
+
+  char point_name[64];
+  sprintf(point_name, "%s/%d", info->dpoint_prefix, info->line);
   
   while (1) {
-    nfds = epoll_wait(epfd, &ev, 1, 20000);
+    nfds = epoll_wait(info->epfd, &ev, 1, 20000);
     if (nfds != 0) {
       struct gpioevent_data edata;
-      res = read(req.fd, &edata, sizeof(edata));
-      printf("%u,%llu\n", edata.id, edata.timestamp);
+      nread = read(info->req.fd, &edata, sizeof(edata));
+
+      int status = 1-(edata.id-1); /* verify this! */
+      
+      ds_datapoint_t *dp = dpoint_new(point_name,
+				      tclserver_now(info->tclserver),
+				      DSERV_INT, sizeof(int),
+				      (unsigned char *) &status);
+      tclserver_set_point(info->tclserver, dp);
+      
+      //      printf("%s: %u,%llu [%llu]\n", point_name,
+      //	     edata.id, edata.timestamp, tclserver_now(info->tclserver));
     }
   }
 }
 
+static int shutdown_input_thread(gpio_input_t *ireq)
+{
+  pthread_cancel(ireq->input_thread_id);
+  close(ireq->epfd);
+  close(ireq->req.fd);
+  return 0;
+}
 
-int gpio_line_request_input_command(ClientData data,
+static int gpio_line_request_input_command(ClientData data,
 				    Tcl_Interp *interp,
 				    int objc, Tcl_Obj *objv[])
 {
@@ -101,7 +126,7 @@ int gpio_line_request_input_command(ClientData data,
     return TCL_ERROR;
   }
   if (offset >= info->nlines) {
-    Tcl_AppendResult(interp, "invalid line specified for output (",
+    Tcl_AppendResult(interp, "invalid line specified for input (",
 		     Tcl_GetString(objv[1]), ")",
 		     NULL);
     return TCL_ERROR;
@@ -111,27 +136,31 @@ int gpio_line_request_input_command(ClientData data,
     // allow specification of event type
   }
   
-  struct gpio_input_t *ireq = info->event_requests[offset];
+  gpio_input_t *ireq = info->input_requests[offset];
   if (ireq) {		/* already opened, so close */
-    /* clean up! */
-    close(ireq->req.fd);
-    /* shutdown receive thread */
+    shutdown_input_thread(ireq);
+    /* wait for thread to shutdown */
+    pthread_join(ireq->input_thread_id, NULL);
   }
   else {
-    ireq = info->input_requests[offset] = (struct gpioinput_request *)
-      calloc(1, sizeof(struct gpioinput_request));
+    ireq = info->input_requests[offset] = (gpio_input_t *)
+      calloc(1, sizeof(gpio_input_t));
   }
 
-  ireq->req.lineoffsets[0] = offset;
+  ireq->req.lineoffset = offset;
   ireq->req.handleflags = GPIOHANDLE_REQUEST_INPUT;
-  ireq->req.eventflags = GPIOEVENT_REQUEST_RISING_EDGE;
+  ireq->req.eventflags = GPIOEVENT_REQUEST_BOTH_EDGES;
   strncpy(ireq->req.consumer_label, "dserv input",
 	  sizeof(ireq->req.consumer_label));
-    
+
+  ireq->tclserver = info->tclserver;
+  ireq->dpoint_prefix = info->dpoint_prefix; /* belongs to global */
+  ireq->line = offset;
+  
   int ret = ioctl(info->fd, GPIO_GET_LINEEVENT_IOCTL, &ireq->req);
   if (ret != -1) {
     if (pthread_create(&ireq->input_thread_id, NULL, input_thread,
-		       (void *) req)) {
+		       (void *) ireq)) {
       return TCL_ERROR;
     }
   }
@@ -140,8 +169,80 @@ int gpio_line_request_input_command(ClientData data,
   return TCL_OK;
 }
 
+
+static int gpio_line_release_input_command(ClientData data,
+				    Tcl_Interp *interp,
+				    int objc, Tcl_Obj *objv[])
+{
+  gpio_info_t *info = (gpio_info_t *) data;
+  int offset = 0;
+  
+  if (info->fd < 0) {
+    return TCL_OK;
+  }
+  
+  if (objc < 2) {
+    Tcl_WrongNumArgs(interp, 1, objv, "offset");
+    return TCL_ERROR;
+  }
+
+  /* check all args first */
+  if (Tcl_GetIntFromObj(interp, objv[1], &offset) != TCL_OK) {
+    return TCL_ERROR;
+  }
+  if (offset >= info->nlines) {
+    Tcl_AppendResult(interp, "invalid line specified (",
+		     Tcl_GetString(objv[1]), ")",
+		     NULL);
+    return TCL_ERROR;
+  }
+
+  gpio_input_t *ireq = info->input_requests[offset];
+  if (ireq) {		/* already opened, so close */
+    shutdown_input_thread(ireq);
+    /* wait for thread to shutdown */
+    pthread_join(ireq->input_thread_id, NULL);
+    free(ireq);
+    info->input_requests[offset] = NULL;
+    Tcl_SetObjResult(interp, Tcl_NewIntObj(offset));
+  }
+  else {
+    Tcl_SetObjResult(interp, Tcl_NewIntObj(-1));
+  }
+  return TCL_OK;
+}
+
+static int gpio_line_release_all_inputs_command(ClientData data,
+						Tcl_Interp *interp,
+						int objc, Tcl_Obj *objv[])
+{
+  gpio_info_t *info = (gpio_info_t *) data;
+  int offset;
+  gpio_input_t *ireq;
+  int nreleased = 0;
+  
+  if (info->fd < 0) {
+    return TCL_OK;
+  }
+
+  /* loop through all lines and release if had been requested */
+  for (offset = 0; offset < info->nlines; offset++) {
+    ireq = info->input_requests[offset];
+    if (ireq) {		/* already opened, so close */
+      shutdown_input_thread(ireq);
+      /* wait for thread to shutdown */
+      pthread_join(ireq->input_thread_id, NULL);
+      free(ireq);
+      info->input_requests[offset] = NULL;
+      nreleased++;
+    }
+  }
+  Tcl_SetObjResult(interp, Tcl_NewIntObj(nreleased));
+  return TCL_OK;
+}
+
 #else
-int gpio_line_request_input_command(ClientData data,
+static int gpio_line_request_input_command(ClientData data,
 				    Tcl_Interp *interp,
 				    int objc, Tcl_Obj *objv[])
 {
@@ -159,6 +260,29 @@ int gpio_line_request_input_command(ClientData data,
       return TCL_ERROR;
     }
   }
+  return TCL_OK;
+}
+
+static int gpio_line_release_input_command(ClientData data,
+				    Tcl_Interp *interp,
+				    int objc, Tcl_Obj *objv[])
+{
+  int offset;
+  
+  if (objc < 2) {
+    Tcl_WrongNumArgs(interp, 1, objv, "offset");
+    return TCL_ERROR;
+  }  
+  if (Tcl_GetIntFromObj(interp, objv[1], &offset) != TCL_OK) {
+    return TCL_ERROR;
+  }
+  return TCL_OK;
+}
+
+static int gpio_line_release_all_inputs_command(ClientData data,
+				    Tcl_Interp *interp,
+				    int objc, Tcl_Obj *objv[])
+{
   return TCL_OK;
 }
 
@@ -186,8 +310,9 @@ int Dserv_gpio_input_Init(Tcl_Interp *interp)
     return TCL_ERROR;
   }
 
-  tclserver_t *tclserver = tclserver_get();
-
+  g_gpioInfo.tclserver = tclserver_get();
+  g_gpioInfo.dpoint_prefix = "gpio/input";
+    
 #ifdef __linux__
   /*
    * could do this in another function, but for now, just default to
@@ -201,19 +326,24 @@ int Dserv_gpio_input_Init(Tcl_Interp *interp)
     
     if (ret >= 0) {
       g_gpioInfo.nlines = info.lines;
-      g_gpioInfo.line_requests = 
-	(struct gpiohandle_request **) calloc(info.lines,
-					      sizeof(struct gpiohandle_request *));
+      g_gpioInfo.input_requests =
+	(gpio_input_t **) calloc(info.lines, sizeof(struct gpio_input_t *));
     }
     else {
       g_gpioInfo.nlines = 0;
-      g_gpioInfo.line_requests = NULL;
+      g_gpioInfo.input_requests = NULL;
     }
   }
 #endif
   
   Tcl_CreateObjCommand(interp, "gpioLineRequestInput",
 		       (Tcl_ObjCmdProc *) gpio_line_request_input_command,
+		       &g_gpioInfo, NULL);
+  Tcl_CreateObjCommand(interp, "gpioLineReleaseInput",
+		       (Tcl_ObjCmdProc *) gpio_line_release_input_command,
+		       &g_gpioInfo, NULL);
+  Tcl_CreateObjCommand(interp, "gpioLineReleaseAllInputs",
+		       (Tcl_ObjCmdProc *) gpio_line_release_all_inputs_command,
 		       &g_gpioInfo, NULL);
   return TCL_OK;
 }
