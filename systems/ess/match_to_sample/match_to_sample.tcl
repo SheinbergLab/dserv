fc7eb11b8c5a0e5a2d4b348e20a0284ee1523598
David Sheinberg <David_Sheinberg@brown.edu>
2024-07-23 15:15:00 -0400

    use response_timeout variable in wait_for_response

--- systems/ess/match_to_sample/match_to_sample.tcl
+++ systems/ess/match_to_sample/match_to_sample.tcl
@@ -143,7 +143,7 @@ namespace eval match_to_sample {
 	# wait_for_response
 	#
 	$sys add_action wait_for_response {
-	    timerTick 10000
+	    timerTick $response_timeout
 	}
 	$sys add_transition wait_for_response {
 	    if [timerExpired] { return no_response }
