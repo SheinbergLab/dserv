89b1e663dc4f4adc009e92e8b96a4ca57ba1fd74
David Sheinberg <David_Sheinberg@brown.edu>
2024-08-07 23:13:46 -0400

    added test code

--- scripts/tcl/testain.tcl
+++ scripts/tcl/testain.tcl
@@ -1,8 +1,13 @@
+package require dlsh
+package require qpcs
+
 set server localhost
 proc update_eye_position { x y } {
     dl_local coords [dl_create short $y $x]
     set ds [qpcs::dsSocketOpen $::server]
-    qpcs::dsSocketSetData $ds ain/vals $coords
+    for { set i 0 } { $i < 20 } { incr i } {
+	qpcs::dsSocketSetData $ds ain/vals $coords
+    }
     close $ds
 }
 
@@ -14,3 +19,11 @@ proc touch_position { x y } {
     close $ds
 }
 
+proc do_test {} {
+    for { set x 0 } { $x < 4096 } { incr x } {
+	update_eye_position $x 2048
+	puts "$x 2048"
+    }
+}
+    
+do_test
