3440417bfee137002a47fce73a7457d94c533470
David Sheinberg <David_Sheinberg@brown.edu>
2024-08-10 14:19:51 -0400

    initial commit

---
+++ src/TclCommands.h
@@ -0,0 +1,25 @@
+Tcl_Obj *dpoint_to_tclobj(Tcl_Interp *interp,  ds_datapoint_t *dpoint);
+int dserv_get_command(ClientData data, Tcl_Interp * interp, int objc,
+		      Tcl_Obj * const objv[]);
+int now_command(ClientData data, Tcl_Interp * interp, int objc,
+		Tcl_Obj * const objv[]);
+int dserv_setdata_command (ClientData data, Tcl_Interp *interp,
+			   int objc, Tcl_Obj * const objv[]);
+int dserv_setdata64_command (ClientData data, Tcl_Interp *interp,
+			     int objc, Tcl_Obj * const objv[]);
+int dserv_timestamp_command(ClientData data, Tcl_Interp *interp,
+			    int objc, Tcl_Obj * const objv[]);
+int dserv_touch_command(ClientData data, Tcl_Interp * interp, int objc,
+			Tcl_Obj * const objv[]);
+int dserv_set_command(ClientData data, Tcl_Interp * interp, int objc,
+		      Tcl_Obj * const objv[]);
+int dserv_clear_command(ClientData data, Tcl_Interp * interp, int objc,
+			Tcl_Obj * const objv[]);
+int dserv_eval_command(ClientData data, Tcl_Interp * interp, int objc,
+		       Tcl_Obj * const objv[]);
+int process_get_param_command(ClientData data, Tcl_Interp * interp, int objc,
+			      Tcl_Obj * const objv[]);
+int process_set_param_command(ClientData data,
+			      Tcl_Interp * interp, int objc,
+			      Tcl_Obj * const objv[]);
+
