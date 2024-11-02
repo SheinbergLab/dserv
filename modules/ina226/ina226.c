dbebf6ddabac9cdb50686f33005aeac444ea13bb
David Sheinberg <David_Sheinberg@brown.edu>
2024-11-02 18:50:22 -0400

    changed WriteRegister to send as single block and modified trigger function

--- modules/ina226/ina226.c
+++ modules/ina226/ina226.c
@@ -39,6 +39,18 @@
 #include <linux/i2c-dev.h>
 #endif
 
+static  uint8_t INA226_ADDRESS          = {0x40};
+static  uint8_t INA226_CONF_REG         = {0x00}; //Configuration Register
+static  uint8_t INA226_SHUNT_REG        = {0x01}; //Shunt Voltage Register
+static  uint8_t INA226_BUS_REG          = {0x02}; //Bus Voltage Register
+static  uint8_t INA226_PWR_REG          = {0x03}; //Power Register 
+static  uint8_t INA226_CURRENT_REG      = {0x04}; //Current flowing through Shunt
+static  uint8_t INA226_CAL_REG          = {0x05}; //Calibration Register 
+static  uint8_t INA226_MASK_EN_REG      = {0x06}; //Mask/Enable Register 
+static  uint8_t INA226_ALERT_LIMIT_REG  = {0x07}; //Alert Limit Register
+static  uint8_t INA226_MAN_ID_REG       = {0xFE}; //Contains Unique Manbufacturer Identification Number
+static  uint8_t INA226_ID_REG           = {0xFF}; //Contains unique ID
+
 #include <tcl.h>
 
 #include "Datapoint.h"
@@ -75,21 +87,20 @@ int i2cWriteRegister(int i2cfd, uint8_t slaveaddr, uint8_t reg,
 		     uint8_t *buf, int len)
 {
 #ifdef __linux__
-  struct i2c_msg msgs[2];
+  struct i2c_msg msgs[1];
   struct i2c_rdwr_ioctl_data msgset[1];
 
+  uint8_t data[256];
+  data[0] = reg;
+  memcpy(&data[1], buf, len);
+  
   msgs[0].addr = slaveaddr;
   msgs[0].flags = 0;
-  msgs[0].len = 1;
-  msgs[0].buf = &reg;
-
-  msgs[1].addr = slaveaddr;
-  msgs[1].flags = 0;
-  msgs[1].len = len;
-  msgs[1].buf = buf;
+  msgs[0].len = 1+len;
+  msgs[0].buf = data;
 
   msgset[0].msgs = msgs;
-  msgset[0].nmsgs = 2;
+  msgset[0].nmsgs = 1;
   
   if (ioctl(i2cfd, I2C_RDWR, &msgset) < 0) {
     return -1;
@@ -103,7 +114,7 @@ int i2cWriteWord16(int i2cfd, uint8_t slaveaddr, uint8_t reg,
 {
   uint8_t buf[2];
   buf[0] = (data & 0xFF00) >> 8;
-  buf[1] = (data & 0xFF);
+  buf[1] = (data & 0x00FF);
   return i2cWriteRegister(i2cfd, slaveaddr, reg, buf, 2);
 }
 
@@ -127,7 +138,7 @@ typedef struct ina226_config_s {
 
   float shunt_ohms;
   float max_expected_amps;
-  uint8_t current_lsb;
+  float current_lsb;
   uint16_t calibration_value;
   uint8_t config_bytes[2];
   
@@ -164,9 +175,9 @@ static int ina226_trigger_conversion(ina226_config_t *config)
 {
   if (config->fd < 0) return -1;
 
-  // Trigger a single-shot conversion by writing to the configuration register
-  return i2cWriteRegister(config->fd, config->address,
-			  0x00, config->config_bytes, 2);
+  uint16_t val = i2cReadWord16(config->fd, config->address, INA226_MASK_EN_REG); // clears CNVR (Conversion Ready) Flag
+  val = i2cReadWord16(config->fd, config->address, INA226_CONF_REG);
+  return i2cWriteWord16(config->fd, config->address, INA226_CONF_REG, val);
 }
 
 static int ina226_conversion_complete(ina226_config_t *config)
@@ -177,11 +188,20 @@ static int ina226_conversion_complete(ina226_config_t *config)
   return (mask_enable_reg & 0x0008);
 }
 
+static int ina226_write_calibration(ina226_config_t *config)
+{
+  if (config->fd < 0) return 0;
+  uint16_t val = config->calibration_value;
+  
+  return i2cWriteWord16(config->fd, config->address,
+			INA226_CAL_REG, val);
+}
+
 static int ina226_initialize(ina226_info_t *ina226info,
 			     uint8_t address, char *prefix, char *name)
 {
   ina226_config_t *config = NULL;
-  //  if (ina226info->fd < 0) return -1;
+  if (ina226info->fd < 0) return -1;
 
   int slot;
   
@@ -224,7 +244,7 @@ static int ina226_initialize(ina226_info_t *ina226info,
     config = 0x8F 0xFB
   */
   
-  config->config_bytes[0] = 0x8F;
+  config->config_bytes[0] = 0x7F;
   config->config_bytes[1] = 0xFB;
   
   // I2C addresses for INA226 devices
@@ -233,6 +253,7 @@ static int ina226_initialize(ina226_info_t *ina226info,
 
 #ifdef __linux__
   // trigger first conversion
+  ina226_write_calibration(config);
   ina226_trigger_conversion(config);
   config->active = 1;
 #endif
@@ -255,10 +276,19 @@ static float ina226_read_current(ina226_config_t *config)
   // I2C bus not connected
   if (config->fd < 0) return 0.0;
 
-  uint16_t current_raw = i2cReadWord16(config->fd, config->address, 0x04);
+  int current_raw = i2cReadWord16(config->fd, config->address, 0x04);
   if (current_raw > 32767)
     current_raw -= 65536;
   float current = current_raw * config->current_lsb;
+
+  //#define DEBUG
+#ifdef DEBUG
+  for (int i = 0; i < 8; i++) {
+    printf("Reg: %02x = 0x%04x\n",
+	   i,
+	   i2cReadWord16(config->fd, config->address, i));
+  }
+#endif
   return current;
 }
 
@@ -289,7 +319,7 @@ void *acquire_thread(void *arg)
   while (1) {
     s = read(info->timer_fd, &exp, sizeof(uint64_t));
     if (s == sizeof(uint64_t)) {
-      
+
       for (int i = 0; i < MAX_INA226_CONFIGS; i++) {
 	ina226_config_t *cfg = &info->configs[i];
 	if (cfg && cfg->active) {
@@ -334,6 +364,35 @@ static int ina226_add_command (ClientData data, Tcl_Interp *interp,
 			      Tcl_GetString(objv[3]));
   
   Tcl_SetObjResult(interp, Tcl_NewIntObj(result));
+  return TCL_OK;
+}
+
+static int ina226_write_reg_command (ClientData data, Tcl_Interp *interp,
+				     int objc, Tcl_Obj *objv[])
+{
+  ina226_info_t *info = (ina226_info_t *) data;
+  int address;
+  int reg;
+  int value;
+  uint8_t buf[3];
+
+  if (objc < 4) {
+    Tcl_WrongNumArgs(interp, 1, objv, "address reg val");
+    return TCL_ERROR;
+  }
+  if (Tcl_GetIntFromObj(interp, objv[1], &address) != TCL_OK)
+    return TCL_ERROR;
+  if (Tcl_GetIntFromObj(interp, objv[2], &reg) != TCL_OK)
+    return TCL_ERROR;
+  if (Tcl_GetIntFromObj(interp, objv[3], &value) != TCL_OK)
+    return TCL_ERROR;
+
+  buf[0] = reg;
+  buf[1] = ((value & 0xFF00) >> 8);
+  buf[2] = (value & 0x00FF);
+
+  i2cWriteWord16(info->fd, address, reg, value);
+
   return TCL_OK;
 }
 
@@ -411,6 +470,10 @@ EXPORT(int,Dserv_ina_Init) (Tcl_Interp *interp)
   
   Tcl_CreateObjCommand(interp, "ina226Add",
 		       (Tcl_ObjCmdProc *) ina226_add_command,
+		       (ClientData) &g_ina226Info,
+		       (Tcl_CmdDeleteProc *) NULL);
+  Tcl_CreateObjCommand(interp, "ina226WriteReg",
+		       (Tcl_ObjCmdProc *) ina226_write_reg_command,
 		       (ClientData) &g_ina226Info,
 		       (Tcl_CmdDeleteProc *) NULL);
   return TCL_OK;
