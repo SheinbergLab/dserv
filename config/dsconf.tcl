813523967fb9f3d3c567f84ffcd887ddb1c5e888
David Sheinberg <David_Sheinberg@brown.edu>
2024-11-05 12:35:53 -0500

    start default system

--- config/dsconf.tcl
+++ config/dsconf.tcl
@@ -131,3 +131,6 @@ connect_touchscreen
 # connect to battery power circuits
 ina226Add 0x45 system/battery 12v
 ina226Add 0x44 system/battery 24v
+
+# and finally load a default system
+ess::load_system
