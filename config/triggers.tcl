2759ea78f5bde7d97ad0407818fd6d03b04aa572
David Sheinberg <David_Sheinberg@brown.edu>
2024-11-14 13:35:45 -0500

    changed qpcs domain to ess

--- config/triggers.tcl
+++ config/triggers.tcl
@@ -5,15 +5,15 @@ puts "initializing qpcsvars"
 proc update_dio { name data } {
     set eds [expr { [lindex $data 0] & 0xFFFFFFFF}]
     set lev [expr { [lindex $data 1] & 0xFFFFFFFF}]
-    dservSet qpcs/dio "$eds $lev"
+    dservSet ess/dio "$eds $lev"
 }
 
 proc update_joystick { name data } {
-    dservSet qpcs/joystick [expr {31-$data}]
+    dservSet ess/joystick [expr {31-$data}]
 }
 
 proc update_touch { name data } {
-    dservSet qpcs/touch "[lindex $data 0] [lindex $data 1]"
+    dservSet ess/touch "[lindex $data 0] [lindex $data 1]"
 }
 
 proc update_ain { name data } {
@@ -23,7 +23,7 @@ proc update_ain { name data } {
     set v2 [expr { [lindex $data 1] & 0xFFFF}]
     set v2a [format %.3f [expr {(($v2-2048.)/$em_pnts_per_deg_y)}]]
     set v1a [format %.3f [expr {((2048.-$v1)/$em_pnts_per_deg_x)}]]
-    dservSet qpcs/em_pos "$v2 $v1 $v2a $v1a"
+    dservSet ess/em_pos "$v2 $v1 $v2a $v1a"
 }
 
 proc update_events { name data } {
@@ -33,75 +33,83 @@ proc update_events { name data } {
 
     switch $type { 
 	3 { switch $sub {
-	    0 { dservSet qpcs/state Running }
-	    1 { dservSet qpcs/state Stopped }
-	    4 { dservSet qpcs/state Stopped }
-	    2 { dservSet qpcs/reset 1 }
+	    0 { dservSet ess/state Running }
+	    1 { dservSet ess/state Stopped }
+	    4 { dservSet ess/state Stopped }
+	    2 { dservSet ess/reset 1 }
 	  }
 	}
 	7 { switch $sub {
-	    0 { dservSet qpcs/state Stopped }
-	    1 { dservSet qpcs/state Running }
-	    2 { dservSet qpcs/state Inactive }
+	    0 { dservSet ess/state Stopped }
+	    1 { dservSet ess/state Running }
+	    2 { dservSet ess/state Inactive }
 	  }
 	}
 	18 { switch $sub {
-	    0 { dservSet qpcs/system $data }
-	    1 { dservSet qpcs/subject $data }
-	    2 { dservSet qpcs/protocol [lindex [split $data :] 1] }
-	    3 { dservSet qpcs/variant [lindex [split $data :] 2] }
+	    0 { dservSet ess/system $data }
+	    1 { dservSet ess/subject $data }
+	    2 { dservSet ess/protocol [lindex [split $data :] 1] }
+	    3 { dservSet ess/variant [lindex [split $data :] 2] }
 	}
-	    dservSet qpcs/state Stopped
+	    dservSet ess/state Stopped
 	}
 	19 {
-	    dservSet qpcs/obs_active 1
-	    dservSet qpcs/obs_id [lindex $data 0]
-	    dservSet qpcs/obs_total [lindex $data 1]
+	    dservSet ess/obs_active 1
+	    dservSet ess/obs_id [lindex $data 0]
+	    dservSet ess/obs_total [lindex $data 1]
 	}
 	20 {
-	    dservSet qpcs/obs_active 0
+	    dservSet ess/obs_active 0
 	}
 	29 {
 	    if { $sub == 1 } {
-		dservSet qpcs/stimtype [lindex $data 0]
+		dservSet ess/stimtype [lindex $data 0]
 	    }
 	}
     }
 }
 
+proc update_state { name data } {
+    if { $data == "Running" } {
+	dservSet ess/running 1
+    } else {
+	dservSet ess/running 0
+    }
+}
+
 proc update_ain_window_settings { name data } {
-    dservSet qpcs/em_region_setting $data
+    dservSet ess/em_region_setting $data
 }
 
 proc update_ain_window_status { name data } {
-    dservSet qpcs/em_region_status $data
+    dservSet ess/em_region_status $data
 }
 
 proc update_touch_window_settings { name data } {
-    dservSet qpcs/touch_region_setting $data
+    dservSet ess/touch_region_setting $data
 }
 
 proc update_touch_window_status { name data } {
-    dservSet qpcs/touch_region_status $data
+    dservSet ess/touch_region_status $data
 }
 
 proc init_vars { } {
-    dservSet qpcs/time [now]
-    dservSet qpcs/dio "0 0"
-    dservSet qpcs/em_pos "2048 2048 0. 0."
-    dservSet qpcs/state "inactive"
+    dservSet ess/time [now]
+    dservSet ess/dio "0 0"
+    dservSet ess/em_pos "2048 2048 0. 0."
+    dservSet ess/state "Inactive"
     
-    dservSet qpcs/name classify
-    dservSet qpcs/executable ess
-    dservSet qpcs/remote 100.0.0.20
-    dservSet qpcs/subject tiger
+    dservSet ess/name classify
+    dservSet ess/executable ess
+    dservSet ess/remote 100.0.0.20
+    dservSet ess/subject tiger
     
-    dservSet qpcs/obs_active 0
-    dservSet qpcs/obs_id 1
-    dservSet qpcs/obs_total 100
+    dservSet ess/obs_active 0
+    dservSet ess/obs_id 1
+    dservSet ess/obs_total 100
     
-    dservSet qpcs/datafile ""
-    dservSet qpcs/lastfile ""
+    dservSet ess/datafile ""
+    dservSet ess/lastfile ""
 }
 
 
@@ -119,6 +127,7 @@ triggerAdd proc/windows/settings       1  update_ain_window_settings
 triggerAdd proc/touch_windows/status   1  update_touch_window_status
 triggerAdd proc/touch_windows/settings 1  update_touch_window_settings
 triggerAdd eventlog/events             1  update_events
+triggerAdd ess/state                   1  update_state
 
 set path [file dir [info nameofexecutable]]
 
