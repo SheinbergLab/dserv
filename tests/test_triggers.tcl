170c05bc9f34ec0b0f555c8f3ecfa4438b74cb3d
Ben Heasly <benjamin.heasly@gmail.com>
2025-01-22 12:58:47 -0500

    trigger_script test use a single, multiline regex.

--- tests/test_triggers.tcl
+++ tests/test_triggers.tcl
@@ -2,9 +2,7 @@ puts "Start trigger test."
 
 # This handler will be called by dserv when values "test/foo" and "test/bar" are set.
 proc test_handler { name data } {
-    # Confirm we see expected values at the time of set and at the time of handling.
-    # TODO: there's an unexpeted, non-printing character at the end of $name.
-    # shell ignores this but ctest treats this like newline, breaking regex tests.
+    # Print the value that was set as well as the current value at handling time.
     puts "Handle $name: event = $data, latest = [dservGet $name]"
 }
 
@@ -12,13 +10,12 @@ triggerRemoveAll
 dservSet test/foo 0
 dservSet test/bar 0
 
-# Bind our handler script to the value names we chose, to alert on every set (every 1 call).
-# TODO: can we register a handler with a wildcard?
-# triggerAdd test/* 1 test_handler
+# Bind our handler script to the value name test/foo and test/bar, to alert on "every 1" call ie always.
+# TODO: something amiss when registering handler with wildcard, as in triggerAdd test/* 1 test_handler
 triggerAdd test/foo 1 test_handler
 triggerAdd test/bar 1 test_handler
 
-# Expect sets 42 43 to trigger test_handler.
+# Expect sets 42 and 43 to trigger our test_handler.
 dservSet test/foo 42
 dservSet test/bar 43
 
