abec30651b4ce80f6b2bcb08601a24669ba84c99
Ben Heasly <benjamin.heasly@gmail.com>
2025-01-23 16:49:19 -0500

    Refactor datapoint serialization test to use GoogleTest.

--- tests/datapoint_serialization.cpp
+++ tests/datapoint_serialization.cpp
@@ -1,64 +1,47 @@
-#include <iostream>
+#include <gtest/gtest.h>
 #include <stdio.h>
 #include <string.h>
 
+#include <iostream>
+
 #include "Datapoint.h"
 
-// Perhaps we should adopt Google Test, with handy assertions, for tests like this.
-// https://google.github.io/googletest/quickstart-cmake.html
+TEST(DatapointSerialization, RoundTrip) {
+  // Construct a sample datapoint.
+  char varname[] = "test/datapoint";
+  uint64_t timestmap = 42;
+  double data = 1234.4567;
+  ds_datapoint_t *datapoint = dpoint_new(
+      varname, timestmap, DSERV_DOUBLE, sizeof(double), (unsigned char *)&data);
 
-int main(int argc, char *argv[])
-{
-    // Construct a sample datapoint.
-    char varname[] = "test/datapoint";
-    uint64_t timestmap = 42;
-    double data = 1234.4567;
-    ds_datapoint_t *datapoint = dpoint_new(
-        varname,
-        timestmap,
-        DSERV_DOUBLE,
-        sizeof(double),
-        (unsigned char *)&data);
+  // By convention, datapoint varlen should be the length of varname, plus 1.
+  // This way the buffer includes the null terminator.
+  EXPECT_EQ(datapoint->varlen, strlen(varname) + 1);
+  EXPECT_STREQ(datapoint->varname, varname);
 
-    // By convention, datapoint varlen should be the length of varname, plus 1.
-    // This way the buffer includes the null terminator.
-    if(datapoint->varlen != strlen(varname) + 1)
-        exit(1);
-    if (strcmp(datapoint->varname, varname))
-        exit(1);
+  // Serialize, then deserialize the datapoint.
+  int buffer_size = dpoint_binary_size(datapoint);
+  unsigned char *buffer = (unsigned char *)malloc(buffer_size);
+  int serialized_size = dpoint_to_binary(datapoint, buffer, &buffer_size);
 
-    // Serialize, then deserialize the datapoint.
-    int buffer_size = dpoint_binary_size(datapoint);
-    unsigned char *buffer = (unsigned char *)malloc(buffer_size);
-    int serialized_size = dpoint_to_binary(datapoint, buffer, &buffer_size);
+  dpoint_free(datapoint);
 
-    if (buffer_size != serialized_size)
-        exit(1);
+  EXPECT_EQ(buffer_size, serialized_size);
 
-    ds_datapoint_t *datapoint_2 = dpoint_from_binary((char *)buffer, serialized_size);
+  ds_datapoint_t *datapoint_2 =
+      dpoint_from_binary((char *)buffer, serialized_size);
 
+  free(buffer);
 
-    // The deserialized datapoint should match the original.
-    if (strcmp(datapoint_2->varname, varname))
-        exit(1);
+  // The deserialized datapoint should match the original.
+  EXPECT_EQ(datapoint_2->varlen, strlen(varname) + 1);
+  EXPECT_STREQ(datapoint_2->varname, varname);
+  EXPECT_EQ(datapoint_2->timestamp, timestmap);
+  EXPECT_EQ(datapoint_2->data.type, DSERV_DOUBLE);
+  EXPECT_EQ(datapoint_2->data.len, sizeof(double));
 
-    if(datapoint_2->varlen != strlen(varname) + 1)
-        exit(1);
+  double data_2 = *(double *)(datapoint_2->data.buf);
+  EXPECT_EQ(data_2, data);
 
-    if(datapoint_2->timestamp != timestmap)
-        exit(1);
-
-    if(datapoint_2->data.type != DSERV_DOUBLE)
-        exit(1);
-
-    if(datapoint_2->data.len != sizeof(double))
-        exit(1);
-
-    double data_2 = *(double *)(datapoint_2->data.buf);
-    if(data_2 != data)
-        exit(1);
-
-    free(buffer);
-    dpoint_free(datapoint_2);
-    dpoint_free(datapoint);
+  dpoint_free(datapoint_2);
 }
