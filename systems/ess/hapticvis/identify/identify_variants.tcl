25ec34f92e9f2f329e45da7df7a417646ac00712
David Sheinberg <David_Sheinberg@brown.edu>
2025-02-20 12:45:29 -0500

    initial commit

--- systems/ess/hapticvis/identify/identify_variants.tcl
+++ systems/ess/hapticvis/identify/identify_variants.tcl
@@ -6,168 +6,164 @@
 #   variant dictionary
 #
 
+package require haptic
+
 namespace eval hapticvis::identify {
 
     # state system parameters
     variable params_defaults { delay_time 100 }
-    
+
     # variant description
-    # find database
-    set db {}
-    set paths [list \
-		   /shared/lab/stimuli/grasp/objects2.db \
-		   ${::ess::system_path}/$::ess::current(project)/hapticvis/identify/data/objects2.db]
-    foreach p $paths {
-	if [file exists $p] { set db $p; break }
-    }
 
     set subject_ids [dl_tcllist [dl_fromto 0 30]]
     set subject_sets [dl_tcllist [dl_fromto 0 4]]
 
+    # could be options...
+    variable n_per_set 4
+    variable n_sets    4
+    
     variable variants {
-	visual {
-	    description "learn visual objects"	    
-	    loader_proc setup_trials
-	    loader_options {
-		dbfile { { objects2 $db } }
-		trial_type visual
+        visual {
+            description "learn visual objects"      
+            loader_proc setup_trials
+            loader_options {
 		subject_id { $subject_ids }
-	        subject_set { $subject_sets }
+		subject_set { $subject_sets }
+		trial_type visual
 		shape_scale { 3 4 5 6 }
 		n_rep { 2 4 6 8 10 }
-		rotations { {single {0}} {three {-120 0 120}} {four {0 90 180 270}} } 
-	    }
-	}
+		rotations {
+		    {single {0}} {three {-120 0 120}} {four {0 90 180 270}}
+                }
+            }
+        }
     }
 
     # substitute variables in variant description above
     set variants [subst $variants]
-    
-    proc variants_init { s } {
 
+    proc variants_init { s } {
+	
         $s add_method visual_init {} {
             rmtSend "setBackground 100 100 100"
         }
 
-	#
-	# extract coords for a given shape
-	#
-	$s add_method get_shape_coords { db shapeID } {
-    
-	    # extract shape coords for this shape
-	    set sqlcmd { SELECT x,y FROM shapeTable${shapeID} }
-	    set coords [$db eval $sqlcmd]
-
-	    # do we need to rotate?
+	$s add_method add_subject_blocks { subject_id block_id dbfile shapedb } {
+	    set row [dl_find [trialdb:subject $subject_id]]
+	    if { $row >= 0 } { return }
 	}
-	
-	$s add_method open_shape_db { dbname srcfile } {
-	    # create an in-memory database loaded with graspdb tables
-	    package require sqlite3
+			 
+        $s add_method setup_trials { subject_id subject_set trial_type shape_scale n_rep rotations } {
+	    # find database
+	    set db {}
+	    set p ${::ess::system_path}/$::ess::current(project)/hapticvis/db
+	    variable shapedb_file [file join $p shape_db]
+	    variable trialdb_file [file join $p trial_db]
+
+
+            # build our stimdg
+            if { [dg_exists stimdg] } { dg_delete stimdg }
+            set g [dg_create stimdg]
+            dg_rename $g stimdg
+
+	    # shape coords are in shapedb_file
+            if {![file exists $shapedb_file]} { error "db file not found" }
+	    if { [dg_exists shape_db] } { dg_delete shape_db }
+	    dg_rename [dg_read $shapedb_file] shape_db
+
+	    # trial info in trialdb_file
+	    if { [dg_exists trialdb] } { dg_delete trialdb }
+	    dg_rename [dg_read $trialdb_file] trialdb
+
+	    set targets trialdb:target_ids:$row:$subject_set
+	    set dists   trialdb:dist_ids:$row:$subject_set
+
+            dl_set stimdg:stimtype         [dl_ilist]
+
+            dl_set stimdg:trial_type       [dl_slist]
+            dl_set stimdg:subject_id       [dl_ilist]
+            dl_set stimdg:subject_set      [dl_ilist]
+            dl_set stimdg:shape_set        [dl_ilist]
+            dl_set stimdg:shape_set_size   [dl_ilist]
+            dl_set stimdg:shape_id         [dl_ilist]
+            dl_set stimdg:shape_coord_x    [dl_ilist]
+            dl_set stimdg:shape_coord_y    [dl_ilist]
+            dl_set stimdg:shape_center_x   [dl_flist]
+            dl_set stimdg:shape_center_y   [dl_flist]
+            dl_set stimdg:shape_rot_deg_cw [dl_flist]
+            dl_set stimdg:shape_scale      [dl_flist]
+            dl_set stimdg:shape_filled     [dl_ilist]
+            dl_set stimdg:correct_choice   [dl_ilist]
+            dl_set stimdg:n_choices        [dl_ilist]
+            dl_set stimdg:choice_centers   [dl_llist]
+            dl_set stimdg:choice_scale     [dl_flist]
+            dl_set stimdg:is_cued          [dl_ilist]
+            dl_set stimdg:cued_choices     [dl_llist]
+            dl_set stimdg:feedback_type    [dl_slist]
+
+            # go into table and find info about sets/subject
+	    set shape_ids [dl_tcllist $targets]
+
+            # get coords for each shape
+	    dl_local shape_inds [haptic::get_shape_indices shape_db:id $shape_ids]
+	    dl_local coord_x [dl_choose shape_db:x $shape_inds]
+	    dl_local coord_y [dl_choose shape_db:y $shape_inds]
 	    
-	    
-	    return $dbname
-	}
-	
-	$s add_method setup_trials { dbfile trial_type subject_id subject_set shape_scale n_rep rotations } {
-	    package require sqlite3
-	    
-	    # build our stimdg
-	    if { [dg_exists stimdg] } { dg_delete stimdg }
-	    set g [dg_create stimdg]
-	    dg_rename $g stimdg
+            # total number of trials
+            set n_rotations [llength $rotations]
+            set n_shapes [llength $shape_ids]
+            set n_obs [expr {$n_rep * $n_rotations * $n_shapes}]
 
-	    if {![file exists $dbfile]} { error "db file not found" }
-	    sqlite3 hapticvis_shapedb $dbfile -readonly 1 
-	    
-	    dl_set stimdg:stimtype         [dl_ilist]
-	    
-	    dl_set stimdg:trial_type       [dl_slist]
-	    dl_set stimdg:subject_id       [dl_ilist]
-	    dl_set stimdg:subject_set      [dl_ilist]
-	    dl_set stimdg:shape_set        [dl_ilist]
-	    dl_set stimdg:shape_set_size   [dl_ilist]
-	    dl_set stimdg:shape_id         [dl_ilist]
-	    dl_set stimdg:shape_coord_x    [dl_ilist]
-	    dl_set stimdg:shape_coord_y    [dl_ilist]
-	    dl_set stimdg:shape_center_x   [dl_flist]
-	    dl_set stimdg:shape_center_y   [dl_flist]
-	    dl_set stimdg:shape_rot_deg_cw [dl_flist]
-	    dl_set stimdg:shape_scale      [dl_flist]
-	    dl_set stimdg:shape_filled     [dl_ilist]
-	    dl_set stimdg:correct_choice   [dl_ilist]
-	    dl_set stimdg:n_choices        [dl_ilist]
-	    dl_set stimdg:choice_centers   [dl_llist]
-	    dl_set stimdg:choice_scale     [dl_flist]
-	    dl_set stimdg:is_cued          [dl_ilist]
-	    dl_set stimdg:cued_choices     [dl_llist]
-	    dl_set stimdg:feedback_type    [dl_slist]
+            set is_cued      0
+            set shape_filled 1
 
+            set shape_reps [expr {$n_rep*$n_rotations}]
+            dl_local shape_id [dl_repeat [dl_ilist {*}$shape_ids] $shape_reps]
 
-	    # go into table and find info about sets/subject
-	    set shape_ids { 2630 2631 2632 2639 }
+            set choice_ecc 5
+            set choice_scale 1.5
+            set n_choices $n_shapes
+            dl_local choice_angles \
+            [dl_mult [expr (2*$::pi)/$n_choices] [dl_fromto 0 $n_choices]]
 
-	    # get coords for each shape
-	    dl_local coord_x [dl_llist]
-	    dl_local coord_y [dl_llist]
-	    foreach s $shape_ids {
-		set c [hapticvis_shapedb eval "SELECT x,y FROM shapeTable${s}"]
-		dl_local reshaped [dl_transpose [dl_reshape [dl_flist {*}$c] - 2]]
-		dl_append $coord_x $reshaped:0
-		dl_append $coord_y $reshaped:1
-	    }
+            dl_local choice_center_x \
+              [dl_mult [dl_cos $choice_angles] $choice_ecc]
+            dl_local choice_center_y \
+              [dl_mult [dl_sin $choice_angles] $choice_ecc]
+            dl_local choice_centers \
+              [dl_llist \
+                [dl_transpose [dl_llist $choice_center_x $choice_center_y]]]
 
-	    # total number of trials
+            dl_set stimdg:stimtype     [dl_fromto 0 $n_obs]
+            dl_set stimdg:trial_type   [dl_repeat [dl_slist $trial_type] $n_obs]
+            dl_set stimdg:subject_id   [dl_repeat $subject_id $n_obs]
+            dl_set stimdg:subject_set  [dl_repeat $subject_set $n_obs]
 
-	    set n_rotations [llength $rotations]
-	    set n_shapes [llength $shape_ids]
-	    set n_obs [expr {$n_rep * $n_rotations * $n_shapes}]
+            dl_set stimdg:shape_set      [dl_repeat [dl_ilist -1] $n_obs]
+            dl_set stimdg:shape_set_size [dl_repeat $n_shapes $n_obs]
+            dl_set stimdg:shape_id       $shape_id
+            dl_set stimdg:shape_coord_x  [dl_repeat $coord_x $shape_reps]
+            dl_set stimdg:shape_coord_y  [dl_repeat $coord_y $shape_reps]
+            dl_set stimdg:shape_center_x [dl_zeros $n_obs.]
+            dl_set stimdg:shape_center_y [dl_zeros $n_obs.]
+            dl_set stimdg:shape_rot_deg_cw \
+              [dl_replicate [dl_flist {*}$rotations] [expr $n_rep*$n_shapes]]
+            dl_set stimdg:shape_scale    [dl_repeat $shape_scale $n_obs]
+            dl_set stimdg:shape_filled   [dl_repeat $shape_filled $n_obs]
+            dl_set stimdg:correct_choice \
+              [dl_repeat [dl_series 1 $n_shapes] $shape_reps]
+            dl_set stimdg:n_choices      [dl_repeat $n_choices $n_obs]
+            dl_set stimdg:choice_centers [dl_repeat $choice_centers $n_obs]
+            dl_set stimdg:choice_scale   [dl_repeat $choice_scale $n_obs]
+            dl_set stimdg:is_cued        [dl_repeat $is_cued $n_obs]
+            dl_set stimdg:cued_choices   [dl_repeat [dl_llist] $n_obs]
+            dl_set stimdg:feedback_type  [dl_repeat [dl_slist color] $n_obs]
+            dl_set $g:remaining [dl_ones $n_obs]
 
-	    set is_cued      0
-	    set shape_filled 1
-	    
-	    set shape_reps [expr {$n_rep*$n_rotations}]
-	    dl_local shape_id [dl_repeat [dl_ilist {*}$shape_ids] $shape_reps]
+            # close the db
+	    dg_delete shape_db
 
-	    set choice_ecc 5
-	    set choice_scale 1.5
-	    set n_choices $n_shapes
-	    dl_local choice_angles \
-		[dl_mult [expr (2*$::pi)/$n_choices] [dl_fromto 0 $n_choices]]
-	    
-	    dl_local choice_center_x [dl_mult [dl_cos $choice_angles] $choice_ecc]
-	    dl_local choice_center_y [dl_mult [dl_sin $choice_angles] $choice_ecc]
-	    dl_local choice_centers \
-		[dl_llist [dl_transpose [dl_llist $choice_center_x $choice_center_y]]]
-
-	    dl_set stimdg:stimtype     [dl_fromto 0 $n_obs]
-	    dl_set stimdg:trial_type   [dl_repeat [dl_slist $trial_type] $n_obs]
-	    dl_set stimdg:subject_id   [dl_repeat $subject_id $n_obs]
-	    dl_set stimdg:subject_set  [dl_repeat $subject_set $n_obs]
-
-	    dl_set stimdg:shape_set      [dl_repeat [dl_ilist -1] $n_obs]
-	    dl_set stimdg:shape_set_size [dl_repeat $n_shapes $n_obs]
-	    dl_set stimdg:shape_id       $shape_id
-	    dl_set stimdg:shape_coord_x  [dl_repeat $coord_x $shape_reps]
-	    dl_set stimdg:shape_coord_y  [dl_repeat $coord_y $shape_reps]
-	    dl_set stimdg:shape_center_x [dl_zeros $n_obs.]
-	    dl_set stimdg:shape_center_y [dl_zeros $n_obs.]
-	    dl_set stimdg:shape_rot_deg_cw [dl_replicate [dl_flist {*}$rotations] [expr $n_rep*$n_shapes]]
-	    dl_set stimdg:shape_scale    [dl_repeat $shape_scale $n_obs]
-	    dl_set stimdg:shape_filled   [dl_repeat $shape_filled $n_obs]
-	    dl_set stimdg:correct_choice [dl_repeat [dl_series 1 $n_shapes] $shape_reps]
-	    dl_set stimdg:n_choices      [dl_repeat $n_choices $n_obs]
-	    dl_set stimdg:choice_centers [dl_repeat $choice_centers $n_obs]
-	    dl_set stimdg:choice_scale   [dl_repeat $choice_scale $n_obs]
-	    dl_set stimdg:is_cued        [dl_repeat $is_cued $n_obs]
-	    dl_set stimdg:cued_choices   [dl_repeat [dl_llist] $n_obs]
-	    dl_set stimdg:feedback_type  [dl_repeat [dl_slist color] $n_obs]
-	    dl_set $g:remaining [dl_ones $n_obs]
-	    
-	    # close the sqlite3 db
-	    hapticvis_shapedb close
-	    
-	    return $g
-	}
+            return $g
+        }
     }
 }
---
+++ systems/ess/lib/haptic-1.0.tm
@@ -0,0 +1,107 @@
+# -*- mode: tcl -*-
+
+#
+# haptic.tcl
+#   tools for managing haptic shape data
+#
+
+package require dlsh
+package require sqlite3
+
+namespace eval haptic {
+    variable shape_ids "2041 2013 2037 2036 2021 2015 2067 2092 2066 2019 \
+   	                2035 2099 2070 2060 2050 2027 2051 2022 2065 2064 \
+                        2014 2031 2023 2058 2087 2007 2018 2062 2032 2033 \
+                        2030 2053 2016 2069 2024 2082 2074 2102 2061"
+
+    # Given a source shape_list and target shape_ids, return indices of targets
+    proc get_shape_indices { shape_list shape_ids } {
+	dl_local search_for [dl_pack $shape_ids]
+	dl_local finds [dl_findAll $shape_list $search_for]
+	if { $ft == "" } { return }
+	dl_local ft [dl_transpose $finds]
+	dl_local inds [dl_choose $ft:1 $ft:0]
+	if { [dl_length $inds] != [dl_length $search_for] } {
+	    return
+	}
+	dl_return $inds
+    }
+
+    # Create shape database give shape ids by extracting coords
+    proc create_shape_db { db } {
+	variable shape_ids
+	set dg [dg_create]
+	dl_set $dg:id [dl_ilist]
+	dl_set $dg:x  [dl_llist]
+	dl_set $dg:y  [dl_llist]
+
+	set dbfile $db
+	
+	if {![file exists $dbfile]} { error "db file not found" }
+	sqlite3 hapticvis_shapedb $dbfile -readonly 1
+	
+	# get coords for each shape
+	dl_local coord_x [dl_llist]
+	dl_local coord_y [dl_llist]
+	foreach s $shape_ids {
+	    dl_append $dg:id $s
+	    
+	    set c [hapticvis_shapedb eval "SELECT x,y FROM shapeTable${s}"]
+	    dl_local reshaped \
+		[dl_transpose [dl_reshape [dl_flist {*}$c] - 2]]
+	    dl_append $dg:x $reshaped:0
+	    dl_append $dg:y $reshaped:1
+	}
+	
+	# close the sqlite3 db
+	hapticvis_shapedb close
+
+	return $dg
+    }
+
+    # create persistent identity trials for subjects
+    proc add_identity_trials { shapedb_file trialdb_file n { n_per_set 4 } { n_sets 4 } } {
+
+	# shape coords are in shapedb_file
+	if {![file exists $shapedb_file]} { error "db file not found" }
+	if { [dg_exists shapedb] } { dg_delete shapedb }
+	dg_rename [dg_read $shapedb_file] shapedb
+	
+	# trial info in trialdb_file
+	if { [dg_exists trialdb] } { dg_delete trialdb }
+	if {![file exists $trialdb_file]} {
+	    dg_rename [dg_create] trialdb
+	    dl_set trialdb:subject    [dl_ilist]
+	    dl_set trialdb:target_ids [dl_llist]
+	    dl_set trialdb:dist_ids   [dl_llist]
+	} else {
+	    dg_rename [dg_read $trialdb_file] trialdb
+	}
+	
+	for { set subject_id 0 } { $subject_id < $n } { incr subject_id } {
+	    set row [dl_find trialdb:subject $subject_id]
+	    
+	    # only add if row not in table
+	    if { $row < 0 } {
+		dl_local shuffled [dl_shuffle shapedb:id]
+		set ntargs [expr $n_per_set*$n_sets]
+		set ndists $ntargs
+		if { [expr {$ndists+$ntargs>[dl_length shapedb:id]}] } {
+		    error "not enough shapes in db"
+		}
+		dl_local ts [dl_choose $shuffled [dl_fromto 0 $ntargs]]
+		dl_local ds [dl_choose $shuffled \
+				 [dl_fromto $ntargs [expr {$ntargs+$ndists}]]]
+		dl_append trialdb:subject $subject_id
+		dl_append trialdb:target_ids [dl_reshape $ts $n_sets $n_per_set]
+		dl_append trialdb:dist_ids [dl_reshape $ds $n_sets $n_per_set]
+	    }
+	}
+	dg_write trialdb $trialdb_file
+	dg_delete trialdb
+    }
+}
+
+
+
+
