1396a444ab9a9be0bfe3e567ea38f2718cb66bc7
David Sheinberg <David_Sheinberg@brown.edu>
2025-03-03 18:14:16 -0500

    changed use_joystick to default to true

--- systems/ess/hapticvis/identify/identify.tcl
+++ systems/ess/hapticvis/identify/identify.tcl
@@ -12,7 +12,7 @@ namespace eval hapticvis::identify {
 	
 	$s add_param rmt_host     $::ess::rmt_host   stim ipaddr
 	$s add_param juice_ml             0.6       variable float
-	$s add_param use_joystick           0       variable bool
+	$s add_param use_joystick           1       variable bool
 	
 	$s add_variable cur_id            -1
 	$s add_variable target_slot       -1
