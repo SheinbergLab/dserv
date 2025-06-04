d52b7feb315982e48ce649ce9cdeebb546d1e471
David Sheinberg <David_Sheinberg@brown.edu>
2025-06-04 12:28:22 -0400

    made thread safe

--- src/ObjectRegistry.h
+++ src/ObjectRegistry.h
@@ -1,26 +1,49 @@
+#include <unordered_map>
+#include <mutex>
+#include <memory>
+#include <string>
 
+// Thread safe registry with regular mutex
 template<typename T>
 class ObjectRegistry {
 private:
     std::unordered_map<std::string, T*> objects;
+    mutable std::mutex mutex_;
     
 public:
     void registerObject(const std::string& name, T* obj) {
+        std::lock_guard<std::mutex> lock(mutex_);
         objects[name] = obj;
     }
     
     void unregisterObject(const std::string& name) {
+        std::lock_guard<std::mutex> lock(mutex_);
         objects.erase(name);
     }
     
-    T* getObject(const std::string& name) {
+    T* getObject(const std::string& name) const {
+        std::lock_guard<std::mutex> lock(mutex_);
         auto it = objects.find(name);
         return (it != objects.end()) ? it->second : nullptr;
     }
     
-    const std::unordered_map<std::string, T*>& getAllObjects() const {
+    std::unordered_map<std::string, T*> getAllObjects() const {
+        std::lock_guard<std::mutex> lock(mutex_);
         return objects;
     }
     
-    size_t size() const { return objects.size(); }
+    size_t size() const {
+        std::lock_guard<std::mutex> lock(mutex_);
+        return objects.size();
+    }
+    
+    bool exists(const std::string& name) const {
+        std::lock_guard<std::mutex> lock(mutex_);
+        return objects.find(name) != objects.end();
+    }
+    
+    void clear() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        objects.clear();
+    }
 };
