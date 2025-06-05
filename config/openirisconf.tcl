a470f027199cabdaabaa7e7540d399f7a8e238f9
David Sheinberg <David_Sheinberg@brown.edu>
2025-06-05 18:09:30 -0400

    receive fixed length data buffer

--- config/openirisconf.tcl
+++ config/openirisconf.tcl
@@ -11,8 +11,8 @@ package require yajltcl
 
 namespace eval openiris {
     # parameters for converting from raw values to [0,4095] (2^12)
-    variable scale_h 15
-    variable scale_v 15
+    variable scale_h 2
+    variable scale_v 2
     variable offset_h 2000
     variable offset_v 2000
     
@@ -25,21 +25,29 @@ namespace eval openiris {
 	variable to_deg_h 200.
 	variable to_deg_v 200.
 	
-	set d [yajl::json2dict $data]
-	set left [dict get $d Left]
-	set pupil [dict get [dict get $left Pupil] Center]
-	set l_pupil [list [dict get $pupil X] [dict get $pupil Y]] 
-	set crs [dict get $left CRs]
-	set cr1 [lindex $crs 0]
-	set l_cr1 [list [dict get $cr1 X] [dict get $cr1 Y]]
-	set cr4 [lindex $crs 3]
-	set l_cr4 [list [dict get $cr4 X] [dict get $cr4 Y]]
-	
+	#set d [yajl::json2dict $data]
+	#set left [dict get $d Left]
+	#set pupil [dict get [dict get $left Pupil] Center]
+	#set l_pupil [list [dict get $pupil X] [dict get $pupil Y]] 
+	#set crs [dict get $left CRs]
+	#set cr1 [lindex $crs 0]
+	#set l_cr1 [list [dict get $cr1 X] [dict get $cr1 Y]]
+	#set cr4 [lindex $crs 3]
+	#set l_cr4 [list [dict get $cr4 X] [dict get $cr4 Y]]
+
+	lassign $data frame seconds \
+	    l_pupil_x l_pupil_y \
+	    l_cr1_x l_cr1_y \
+	    l_cr4_x l_cr4_y
+
+	set l_pupil "$l_pupil_x $l_pupil_y"
+	set l_cr1 "$l_cr1_x $l_cr1_y"
+	set l_cr4 "$l_cr4_x $l_cr4_y"
 	dservSet openiris/left/pupil $l_pupil
 	dservSet openiris/left/cr1   $l_cr1
 	dservSet openiris/left/cr4   $l_cr4
-	dservSet openiris/left/frame [dict get $left FrameNumber]
-	dservSet openiris/left/time  [dict get $left Seconds]
+	dservSet openiris/left/frame [expr {int($frame)}]
+	dservSet openiris/left/time  $seconds
 	
 	# set ain/vals as shorts to be compatible with other eye inputs
 	dl_local avals [dl_reverse [dl_short \
@@ -52,8 +60,8 @@ namespace eval openiris {
 	dservSetData ain/vals 0 4 $ainvals
 
 	lassign [dl_tcllist $avals] h v
-	set h [expr {2048.-$h}/$to_deg_h]
-	set v [expr {$v-2048.}/$to_deg_y]
+	set h [expr {(2048.-$h)/$to_deg_h}]
+	set v [expr {($v-2048.)/$to_deg_v}]
 
 	dservSet ess/em_pos "[dl_tcllist $avals] $h $v"
     }
