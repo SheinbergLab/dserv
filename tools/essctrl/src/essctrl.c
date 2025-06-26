7b3788123c0611ff37aeda573eb3f97dd8f7b86f
David Sheinberg <David_Sheinberg@brown.edu>
2025-06-26 12:42:23 -0400

    added non-interactive mode

--- tools/essctrl/src/essctrl.c
+++ tools/essctrl/src/essctrl.c
@@ -31,6 +31,31 @@ void completion(const char *buf, linenoiseCompletions *lc) {
     }
 }
 
+void print_usage(char *prgname) {
+    printf("Usage: %s [server] [options]\n", prgname);
+    printf("  server        Server address (default: localhost)\n");
+    printf("  -c command    Execute command and exit\n");
+    printf("  -s service    Target service (ess, msg, db, dserv, stim, pg, git, openiris)\n");
+    printf("  -h            Show this help\n");
+    printf("\nExamples:\n");
+    printf("  %s                    # Interactive mode with localhost\n", prgname);
+    printf("  %s server.example.com # Interactive mode with specific server\n", prgname);
+    printf("  %s -c \"return 100\"        # Execute 'status' command on localhost\n", prgname);
+    printf("  %s -s db -c \"SELECT * FROM users\" # Execute SQL on db service\n", prgname);
+    printf("  %s server.com -s msg -c \"expr 5*5\"    # Send message to specific server\n", prgname);
+}
+
+int get_port_for_service(char *service) {
+    if (!strcmp(service, "ess")) return ESS_PORT;
+    if (!strcmp(service, "msg")) return MSG_PORT;
+    if (!strcmp(service, "db")) return DB_PORT;
+    if (!strcmp(service, "dserv")) return DSERV_PORT;
+    if (!strcmp(service, "stim")) return STIM_PORT;
+    if (!strcmp(service, "pg")) return PG_PORT;
+    if (!strcmp(service, "git")) return GIT_PORT;
+    if (!strcmp(service, "openiris")) return OPENIRIS_PORT;
+    return -1;
+}
 
 char *do_command(char *server, int tcpport, char *line, int n)
 {
@@ -43,7 +68,6 @@ char *do_command(char *server, int tcpport, char *line, int n)
    }
   return resultstr;
 }
-
 
 char *do_msg_command(char *server, int port, char *line, int n)
 {
@@ -63,26 +87,99 @@ char *do_msg_command(char *server, int port, char *line, int n)
   return buf;
 }
 
+int execute_single_command(char *server, int tcpport, char *command) {
+    char *resultstr;
+    
+    if (tcpport == STIM_PORT || tcpport == MSG_PORT) {
+        resultstr = do_msg_command(server, tcpport, command, strlen(command));
+    } else {
+        resultstr = sock_send(server, tcpport, command, strlen(command));
+    }
+    
+    if (resultstr) {
+        if (strlen(resultstr)) {
+            printf("%s\n", resultstr);
+        }
+        if (tcpport == STIM_PORT || tcpport == MSG_PORT) {
+            free(resultstr);
+        }
+        return 0; // Success
+    }
+    
+    fprintf(stderr, "Error: Failed to execute command\n");
+    return 1; // Error
+}
 
 int main (int argc, char *argv[])
 {
   char *line;
   char *prgname = argv[0];
   int async = 0;
-  char *server;
-  int tcpport = ESS_PORT;;
+  char *server = "localhost";
+  int tcpport = MSG_PORT;
   char *resultstr;
-  char *prompt = "ess> ";
+  char *prompt = MSG_PROMPT;
+  char *command_to_execute = NULL;
+  char *target_service = NULL;
+  int interactive_mode = 1;
+  int i;
   
 #ifdef _MSC_VER
   init_w32_socket();
 #endif
 
-  if (argc < 2) {
-    server = "localhost";
+  // Parse command line arguments
+  for (i = 1; i < argc; i++) {
+      if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
+          print_usage(prgname);
+          return 0;
+      } else if (!strcmp(argv[i], "-c")) {
+          if (i + 1 < argc) {
+              command_to_execute = argv[++i];
+              interactive_mode = 0;
+          } else {
+              fprintf(stderr, "Error: -c option requires a command\n");
+              print_usage(prgname);
+              return 1;
+          }
+      } else if (!strcmp(argv[i], "-s")) {
+          if (i + 1 < argc) {
+              target_service = argv[++i];
+          } else {
+              fprintf(stderr, "Error: -s option requires a service name\n");
+              print_usage(prgname);
+              return 1;
+          }
+      } else if (argv[i][0] != '-') {
+          // Assume it's a server name if it doesn't start with -
+          server = argv[i];
+      } else {
+          fprintf(stderr, "Error: Unknown option %s\n", argv[i]);
+          print_usage(prgname);
+          return 1;
+      }
   }
-  else server = argv[1];
   
+  // Set target service port if specified
+  if (target_service) {
+      tcpport = get_port_for_service(target_service);
+      if (tcpport == -1) {
+          fprintf(stderr, "Error: Unknown service '%s'\n", target_service);
+          fprintf(stderr, "Valid services: ess, msg, db, dserv, stim, pg, git, openiris\n");
+          return 1;
+      }
+  }
+  
+  // If command specified, execute it and exit
+  if (command_to_execute) {
+      int result = execute_single_command(server, tcpport, command_to_execute);
+#ifdef _MSC_VER
+      cleanup_w32_socket();
+#endif
+      return result;
+  }
+  
+  // Continue with interactive mode
   linenoiseInstallWindowChangeHandler();
 
   /* Set the completion callback. This will be called every time the
@@ -201,7 +298,7 @@ int main (int argc, char *argv[])
       }
     } else if (!strncmp(line, "/openiris", 9)) {
       if (strlen(line) > 9) {
-	resultstr = do_command(server, GIT_PORT, &line[10], strlen(line)-10);
+	resultstr = do_command(server, OPENIRIS_PORT, &line[10], strlen(line)-10);
 	if (resultstr && strlen(resultstr)) printf("%s\n", resultstr);
       }
       else {
@@ -209,7 +306,7 @@ int main (int argc, char *argv[])
 	prompt = OPENIRIS_PROMPT;
       }
     } else if (line[0] == '/') {
-      printf("Unreconized command: %s\n", line);
+      printf("Unrecognized command: %s\n", line);
     }
     free(line);
   }
