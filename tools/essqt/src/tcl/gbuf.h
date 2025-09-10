482ed4919a958ce6cd7ecea1bc986b4a324ca4b5
David Sheinberg <David_Sheinberg@brown.edu>
2025-09-10 16:38:29 -0400

    updated gbuf.h

--- tools/essqt/src/tcl/gbuf.h
+++ tools/essqt/src/tcl/gbuf.h
@@ -1,6 +1,7 @@
 /**************************************************************************/
 /*      gbuf.h - graphics buffer package for use with cgraph              */
 /*      Created: 4-Apr-94                                                 */
+/*      Refactored for thread safety - all functions now per-context      */
 /**************************************************************************/
 
 #ifndef _GBUF_H_
@@ -10,58 +11,33 @@
 extern "C" {
 #endif
 
-extern char RecordGEvents;
+/* Forward declaration */
+typedef struct CgraphContext CgraphContext;
 
 typedef enum { GBUF_RAW = 1, GBUF_ASCII, GBUF_AI88, 
 	       GBUF_AI3, GBUF_AI = GBUF_AI3, 
 	       GBUF_PS, GBUF_FIG, GBUF_EPS, GBUF_PDF } GbufDumpTypes;
 
-
-typedef struct {
-  int w;
-  int h;
-  int d;
-  unsigned char *data;
-  float x0, y0, x1, y1;
-} GBUF_IMAGE;
-
-typedef struct {
-  int nimages;
-  int maximages;
-  int allocinc;
-  GBUF_IMAGE *images;
-} GBUF_IMAGES;
-
-typedef struct {
-  unsigned char *gbuf;
-  int gbufindex;
-  int gbufsize;
-  GBUF_IMAGES images;
-  int empty;
-  char record_events;  // Per-buffer recording state
-} GBUF_DATA;
-
-#define GB_RECORD_EVENTS(g)  ((g)->record_events)
-#define GB_GBUF(g)          ((g)->gbuf)
-#define GB_GBUFSIZE(g)      ((g)->gbufsize)
-#define GB_GBUFINDEX(g)     ((g)->gbufindex)
-#define GB_IMAGES(g)        ((g)->images)
-#define GB_EMPTY(g)         ((g)->empty)
+/* Macros for accessing gbuf data within context */
+#define GB_RECORD_EVENTS(ctx)  ((ctx)->gbuf_data.record_events)
+#define GB_GBUF(ctx)          ((ctx)->gbuf_data.gbuf)
+#define GB_GBUFSIZE(ctx)      ((ctx)->gbuf_data.gbufsize)
+#define GB_GBUFINDEX(ctx)     ((ctx)->gbuf_data.gbufindex)
+#define GB_IMAGES(ctx)        ((ctx)->gbuf_data.images)
+#define GB_EMPTY(ctx)         ((ctx)->gbuf_data.empty)
+#define GB_APPEND_TIMES(ctx)  ((ctx)->gbuf_data.append_times)
+#define GB_EVENT_TIME(ctx)    ((ctx)->gbuf_data.event_time)
   
 /*
  *  POSTSCRIPT Support
  */
-
 #define PS_AUTOMATIC 0
 #define PS_LANDSCAPE 1
 #define PS_PORTRAIT  2
 
-extern char PS_Orientation;
-
 /* 
  *  EVENT IDS
  */
-
 #define G_HEADER      (0)
 #define G_POINT       (1+G_HEADER)
 #define G_LINE        (1+G_POINT)
@@ -84,6 +60,7 @@ extern char PS_Orientation;
 #define G_LWIDTH      (1+G_POLY)
 #define G_POSTSCRIPT  (1+G_LWIDTH)
 #define G_IMAGE       (1+G_POSTSCRIPT)
+#define G_BACKGROUND  (1+G_IMAGE)
 
 typedef struct _g_header {
   float version;
@@ -137,84 +114,108 @@ typedef struct _g_attr{
 #define GATTR_VAL(a)  ((a)->val)
 
 /*
- * bytes per event
+ * bytes per event - fixed for 64-bit compatibility
  */
-
 #define GHEADER_S     ((int) (sizeof (GHeader)))
 #define GPOINT_S      ((int) (sizeof (GPoint)))
 #define GLINE_S       ((int) (sizeof (GLine)))
 #define GATTR_S       ((int) (sizeof (GAttr)))
-
-/* 
- * For 64bit compatibility we need to hard code these to pick a standard
- * size independent of pointer size (which is, of course, larger on 64 bit).
- * In this case, when we read/write these structs, the pointer contents is
- * not meaningful anyway, and will be replaced with allocated return values
- */
-/*
-#define GPOINTLIST_S  ((int) (sizeof (GPointList)))
-#define GTEXT_S       ((int) (sizeof (GText)))
-*/
 #define GPOINTLIST_S  (8)      /* sizeof(int)+32bit */
 #define GTEXT_S       (16)     /* sizeof(float)*2+sizeof(int)+32bit */
 
+/*
+ * Buffer management functions - all take CgraphContext parameter
+ */
+void gbInitGeventBuffer(CgraphContext *ctx);
+void gbResetGeventBuffer(CgraphContext *ctx);
+  void gbCleanupGeventBuffer(CgraphContext *ctx);
 
 /*
- * External functions
+ * Recording control - per context
  */
+void gbEnableGeventBuffer(CgraphContext *ctx);
+void gbDisableGeventBuffer(CgraphContext *ctx);
+int gbIsRecordingEnabled(CgraphContext *ctx);
 
-GBUF_DATA *gbInitGeventBuffer(GBUF_DATA *gb);
-GBUF_DATA *gbSetGeventBuffer(GBUF_DATA *);
-GBUF_DATA *gbGetGeventBuffer();
+/*
+ * Buffer state - per context
+ */
+int gbIsEmpty(CgraphContext *ctx);
+int gbSize(CgraphContext *ctx);
+int gbSetEmpty(CgraphContext *ctx, int empty);
 
-void gbInitGevents();
-void gbResetGevents();
-void gbCloseGevents();
-void gbRecordDefaults();
+/*
+ * Timing functions - per context (kept for compatibility)
+ */
+void gbEnableGeventTimes(CgraphContext *ctx);
+void gbDisableGeventTimes(CgraphContext *ctx);
+int gbSetTime(CgraphContext *ctx, int time);
+int gbIncTime(CgraphContext *ctx, int time);
 
-void gbEnableGeventBuffer(GBUF_DATA *gb);
-void gbDisableGeventBuffer(GBUF_DATA *gb);
-void gbEnableCurrentBuffer();
-void gbDisableCurrentBuffer();
-void gbResetCurrentBuffer();
-void gbResetGeventBuffer(GBUF_DATA *gb);
-void gbCleanupGeventBuffer(GBUF_DATA *gb);
-void gbFreeImagesBuffer(GBUF_DATA *gb);
-int gbIsRecordingEnabled();
-  
-int gbAddImage(int w, int h, int d, unsigned char *data,
-	       float x0, float y0, float x1, float y1);
-GBUF_IMAGE *gbFindImage(int ref);
-int gbReplaceImage(int ref, int w, int h, int d, unsigned char *data);
-int  gbWriteGevents(char *, int);
-int gbWriteImageFile(FILE *fp);
-int gbReadImageFile(FILE *fp);
-void gbPrintGevents();
-void gbEnableGevents();
-void gbDisableGevents();
-void gbEnableGeventTimes();
-void gbDisableGeventTimes();
-int  gbSetTime(int time);
-int  gbIncTime(int time);
-int  gbPlaybackGevents();
-int  gbClearAndPlayback(void);
-void gbSetPageOrientation(char ori);
-void gbSetPageFill(int fill);
-int  gbIsEmpty();
-  
-void record_gline(char, float, float, float, float);
-void record_gpoint(char, float, float);
-void record_gpoly(char, int, float *);
-void record_gtext(char, float, float, char *);
-void record_gattr(char, int);
-void gbuf_dump(char *data, int nbytes, int type, FILE *outfp);
-int  gbuf_dump_pdf(char *data, int nbytes, char *filename);
+/*
+ * Image management - per context
+ */
+int gbAddImage(CgraphContext *ctx, int w, int h, int d, unsigned char *data,
+               float x0, float y0, float x1, float y1);
+GBUF_IMAGE *gbFindImage(CgraphContext *ctx, int ref);
+int gbReplaceImage(CgraphContext *ctx, int ref, int w, int h, int d, unsigned char *data);
+void gbFreeImage(GBUF_IMAGE *image);  /* This one operates on image directly */
+void gbFreeImagesBuffer(CgraphContext *ctx);
+int gbWriteImageFile(CgraphContext *ctx, FILE *fp);
+int gbReadImageFile(CgraphContext *ctx, FILE *fp);
+
+/*
+ * Buffer cleaning and output - per context
+ */
+int gbCleanGeventBuffer(CgraphContext *ctx);
+int gbPlaybackGevents(CgraphContext *ctx);
+int gbWriteGevents(CgraphContext *ctx, char *filename, int format);
+void gbPrintGevents(CgraphContext *ctx);
+
+/*
+ * Recording functions - all take context parameter
+ */
+void record_gline(CgraphContext *ctx, char type, float x0, float y0, float x1, float y1);
+void record_gpoint(CgraphContext *ctx, char type, float x, float y);
+void record_gpoly(CgraphContext *ctx, char type, int nverts, float *verts);
+void record_gtext(CgraphContext *ctx, char type, float x, float y, char *str);
+void record_gattr(CgraphContext *ctx, char type, int val);
+
+/*
+ * Special functions
+ */
+void gbRecordDefaults(CgraphContext *ctx);
+
+/*
+ * Page setup functions - per context  
+ */
+void gbSetPageOrientation(CgraphContext *ctx, char ori);
+void gbSetPageFill(CgraphContext *ctx, int fill);
+
+/*
+ * Legacy compatibility
+ */
+void gbCloseGevents(CgraphContext *ctx);  /* Wrapper around gbCleanupGeventBuffer */
+
+/*
+ * Utility functions for external dump utilities (these work on raw buffer data)
+ */
+extern void gbuf_dump(CgraphContext *ctx, char *buffer, int n, int type, FILE *fp);
+extern int gbuf_dump_ps(CgraphContext *ctx, unsigned char *gbuf, int bufsize, int type, FILE *OutFP);
+
+  extern int gbuf_dump_ascii(unsigned char *gbuf, int bufsize, FILE *fp);
+extern char *gbuf_dump_ascii_to_string(unsigned char *data, int nbytes);
+extern char *gbuf_dump_json_direct(unsigned char *data, int nbytes);
+extern unsigned char *gbuf_clean(unsigned char *data, int nbytes, int *clean_size);
+
+/* 
+ * Playback functions - these operate on raw buffer data but need context for drawing
+ */
+extern void playback_gbuf(CgraphContext *ctx, unsigned char *data, int nbytes);
+extern void playback_gfile(CgraphContext *ctx, FILE *fp);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
-
-
-
+#endif /* _GBUF_H_ */
