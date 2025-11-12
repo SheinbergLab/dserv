55984015a58bb2ba2f94689bf82b676617d334fa
David Sheinberg <David_Sheinberg@brown.edu>
2025-11-12 15:48:56 -0500

    store frame_id and time as seconds

--- config/emconf.tcl
+++ config/emconf.tcl
@@ -77,10 +77,14 @@ namespace eval em {
 	}
 	set fvals [binary format "f" $pupil_r]
 	dservSetData em/pupil_r 0 2 $fvals
-	
-	set seconds_binary [binary format d $frame_time]
+
+	set frame_id_binary [binary format i $frame_id]
+	dservSetData em/frame_id 0 5 $frame_id_binary
+
+	set frame_seconds [expr {$frame_time/1e6}
+	set seconds_binary [binary format d $frame_seconds]
 	dservSetData em/time 0 3 $seconds_binary
-	
+			  
 	foreach v "blink p1_detected p4_detected" {
 	    set val [binary format c [expr {int([set $v])}]]
 	    dservSetData em/$v 0 0 $val
